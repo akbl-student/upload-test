9cbe0a00812bfae7ce22771aa2b5bccfcc89761d
akbl-student <160585930+akbl-student@users.noreply.github.com>
2024-03-12 14:41:38 +0500

    Add files via upload

---
+++ stripping_Names.ts
@@ -0,0 +1,9 @@
+
+
+const personname :string = '    \n\tshahid Afridi\t\n    ';
+
+console.log(personname);
+
+const without_whitespace :string = personname.trim();
+
+console.log(without_whitespace);
